a3841b9aeb4792fc72128c6e9f86ec053d99536d
Ahmet Burak Biçer <89780902+BurakAhmet@users.noreply.github.com>
2022-02-20 11:49:11 +0300

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,23 @@
+public class FibonacciSeries {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please Enter The n Value: ");
+		int n =input.nextInt(), firstTerm = 0, secondTerm = 1;
+	    System.out.println("Fibonacci Series till " + n + " terms:");
+	    for (int i = 1; i <= n; ++i)
+        {
+	    	if(i!=n)
+		      {
+		    	  System.out.print(firstTerm + ", ");
+		      }
+	    	else
+		      {
+		    	  System.out.print(firstTerm);
+		      }
+	    	int nextTerm = firstTerm + secondTerm;
+	    	firstTerm = secondTerm;
+	    	secondTerm = nextTerm;
+	    }
+	    input.close();
+	}
+}
